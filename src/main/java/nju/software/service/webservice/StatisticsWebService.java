efc1584f5656040cfdd7e637168875f8c51eb644
iaxax <1483809252@qq.com>
2017-07-09 01:24:41 +0800

    修复课程统计不包括外院系的错误

--- src/main/java/nju/software/service/webservice/StatisticsWebService.java
+++ src/main/java/nju/software/service/webservice/StatisticsWebService.java
@@ -1,6 +1,8 @@
 package nju.software.service.webservice;
 
 import nju.software.config.ServerConfig;
+import nju.software.dao.SelectionDao;
+import nju.software.model.Selection;
 import nju.software.model.statistic.CourseInfo;
 import nju.software.model.statistic.ListBean;
 import nju.software.model.statistic.StudentInfo;
@@ -9,10 +11,12 @@ import nju.software.util.XmlUtil;
 import org.apache.axiom.om.OMElement;
 import org.apache.axis2.databinding.utils.BeanUtil;
 import org.apache.axis2.engine.DefaultObjectSupplier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Anson Shaw on 2017/6/25.
@@ -21,6 +25,9 @@ import java.util.LinkedList;
  */
 @Service(value = "statisticsWebService")
 public class StatisticsWebService {
+
+    @Autowired
+    private SelectionDao selectionDao;
 
     @SuppressWarnings("unchecked")
     public ListBean getCourseStatistics() {
@@ -175,6 +182,20 @@ public class StatisticsWebService {
 
         long end = System.currentTimeMillis();
         System.out.println("调用学生统计信息时间：" + (end - start));
+
+        // 添加跨院系选课数量到统计信息上
+        List<StudentInfo> studentInfoList = result.getList();
+        for (StudentInfo info : studentInfoList) {
+            List<Selection> selections = selectionDao.findAllByStudentidAndStudentInstitution(
+                    info.getStudentid(), info.getInstitution()
+            );
+
+            if (selections != null && selections.size() > 0) {
+                System.out.println(info.getInstitution() + ":" + info.getStudentid()
+                    + "--" + selections.size());
+                info.setCourseNum(info.getCourseNum() + selections.size());
+            }
+        }
         return result;
     }
 
